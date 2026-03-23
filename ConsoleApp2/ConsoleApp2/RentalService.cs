b6ad89f1732a257c87e63e96b608803cf82c2bfa
piotrkowskyyy <piotr.kaczynski10@gmail.com>
2026-03-23 22:14:31 +0100

    Rental service class creation

---
+++ ConsoleApp2/ConsoleApp2/RentalService.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp2;
+
+public class RentalService
+{
+    public List<Rental> Rentals = new();
+    public List<Equipment> Inventory = new();
+    
+    public void AddToInventory(Equipment item) => Inventory.Add(item);
+
+    public void Rent(User user, Equipment item, int days)
+    {
+        if (!item.IsAvailable)
+        {
+            throw new Exception($"Equipment {item.Name} is not available");
+        }
+        int activeAmount = Rentals.Count(r => r.Rentee.IdUser == user.IdUser && r.ReturnDate == null);
+        int limit = user.Type == UserType.Student ? 2 : 5;
+
+        if (activeAmount >= limit)
+        {
+            throw new Exception($"User {user.Name} exceeded limit {limit} of possible rentals");
+        }
+
+        item.IsAvailable = false;
+        Rentals.Add(new Rental(user, item, days));
+    }
+}
