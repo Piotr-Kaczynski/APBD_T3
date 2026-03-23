434d59f391e7c0046c1bfb22c7fc57a7e8f4359f
piotrkowskyyy <piotr.kaczynski10@gmail.com>
2026-03-23 22:26:57 +0100

    #1 scenario - user correctly rented an equipment

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -8,5 +8,14 @@ class Program
         var cam = new Camera("Zenit c445", 42, 3.5);
         var laptop = new Laptop("Dell vostro", "Windows 11", 16);
         var student = new Student(Guid.NewGuid(), "Tom", "Cruise");
+        
+        
+        service.AddToInventory(cam);
+        service.AddToInventory(laptop);
+
+        Console.WriteLine("scenario #1: Correcr renting");
+        service.Rent(student, cam, 5);
+        Console.WriteLine($"Rent {cam.Name} to user: {student.Name} {student.LastName}.");
+        
     }
 }
